8b02d20c8b3f387324721cde9c1f55a24ca8dd22
Daniel Ye <danielyeqt@gmail.com>
2022-04-16 07:20:35 -0400

    Basic Routing

--- app/src/index.js
+++ app/src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-class Main extends React.Component{
-  render(){
-    return (
-      <div>
-        Hello World!
-      </div>
-    )
-  }
-}
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import Profile from "./profile.js";
+import Information from "./info.js";
+import Add from "./add.js";
+import Homepage from "./home.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Main />
+  <Router>
+    <Routes>
+      <Route path="/" element={<Homepage />}></Route>
+      <Route path="/profile" element={<Profile />}></Route>
+      <Route path="/add" element={<Add />}></Route>
+      <Route path="/info" element={<Information />}></Route>
+    </Routes>
+  </Router>
 )
