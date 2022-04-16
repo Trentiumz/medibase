3fbae8fc57728ba1eb7434fd1691840612d0f301
Chelsea W <80985676+cheollie@users.noreply.github.com>
2022-04-16 11:38:13 -0400

    homepage things?

--- app/src/home.js
+++ app/src/home.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import TTSIcon from './tts';
+import './home.css';
 
 export default function Homepage(){
     return(
         <div>
+            <nav id="navbar">
+              <div id="subnavbarleft">
+                <img id="logo" src="/assets/logo.png" alt="MediBase"/> 
+              </div>
+              <div id="subnavbarright">
+                <a className="navbar-subtitle-text nav-underline" href="/medication">medication</a>
+                <a className="navbar-subtitle-text nav-underline" href="/schedule">schedule</a>
+                <a className="navbar-subtitle-text nav-underline" href="/login">login</a>
+              </div>
+
+            </nav>
+            <div className="content">
+              <div className="inner-content">
+                <p className="feature-text">All your medication at your fingertips.</p>
+              </div>
+            </div>        
             Hello World!
             <TTSIcon text="Hello World" lang="en-us"/>
         </div>
