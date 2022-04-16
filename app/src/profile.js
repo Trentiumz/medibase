679d9e0ffa0954b2445d5ce4f59d96105ded7ffb
Daniel Ye <danielyeqt@gmail.com>
2022-04-16 14:33:21 -0400

    Add cookies to Profile

--- app/src/profile.js
+++ app/src/profile.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Cookies from 'universal-cookie';
+import { useNavigate } from 'react-router';
 
 export default function Profile(){
+    const cookies = new Cookies();
+    let cur_id = cookies.get('cur-id');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        console.log(cur_id);
+        if(!cur_id){
+            navigate("/login");
+        }
+    })
+
     return(
         <div>
             Profile!
