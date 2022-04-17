db8d024dfb426e10956213aea275333951685d13
Chelsea W <80985676+cheollie@users.noreply.github.com>
2022-04-16 22:42:43 -0400

    clean up!

--- app/src/scan.js
+++ app/src/scan.js
@@ -29,16 +29,16 @@ function GetDIN() {
     return(
         <div class="scan-content">
             <div class="scan-inner-content">
-                <div id="left-column">
+                <div id="scan-left-column">
                   <div class="scan-container">
                     <div class="scan-placeholder"><p class="scan-placeholder-text"><CurLang text="currently not supported" /></p></div>
                     <div class="scan-label"><CurLang text="scan QR code" /></div>
                   </div>
                 </div>
-                <div id="middle-column">
+                <div id="scan-middle-column">
                   <p class="small"><CurLang text="OR" /></p>
                 </div>
-                <div id="right-column">
+                <div id="scan-right-column">
                   <span class="din-label"><CurLang text="enter DIN:" /></span>
                   <div class="din-info"><CurLang text="DIN is the Drug Identification" /><br/><CurLang text="Number on prescription labels." /></div>
                   <form class="input-form" onSubmit={onSubmit}>
@@ -58,7 +58,6 @@ export default function Scan(){
     return(
         <div>
             <Navbar />
-            <img src="app/public/assets/qrcode.png" alt="app/public/assets/logo.png"></img>
             <GetDIN />
         </div>
     );
