bf31d2078804d558933439b3d8fd69bd9418e66a
Chelsea W <80985676+cheollie@users.noreply.github.com>
2022-04-17 13:19:32 -0400

    Update home.js

--- app/src/home.js
+++ app/src/home.js
@@ -7,9 +7,10 @@ export default function Homepage(){
     return(
         <div>
           <Navbar />
-            <div className="content">
-              <div className="inner-content">
+            <div className="home-content">
+              <div className="home-inner-content">
                 <p className="feature-text"><CurLang text="All your medication at your fingertips." /></p>
+                <p className="feature-body"><CurLang text="MediBase is a medication assistant allowing people who have poorer eyesight, don’t understand English well, or have trouble remembering - to safely take medication." /></p>
               </div>
             </div>
             <div className = "test">
